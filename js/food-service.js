736534e5472659669c5c741cdc067488340b2259
KarmazKseniia <karmaz_ksunja@ua.fm>
2014-12-17 00:05:30 +0200

    recipe factory and add recipe controller

--- js/food-service.js
+++ js/food-service.js
@@ -1,21 +1,60 @@
 angular.module('uHealth.food.service', [
   ])
 
-  // A RESTful factory
-  .factory('products',
+  .factory('productFactory',
     [          '$http',
       function ($http) {
-        var products = $http.get('/api/api/v1/product/list').then(function (resp) {
-          if (resp.data.error) {
-            log_error(resp.data.error);
-            return null;
-          }
-          return resp.data;
-        });
-
+        var urlBase = '/api/api/v1/product';
         var factory = {};
-        factory.all = function () {
-          return products;
+
+        factory.getList = function () {
+          return $http.get(urlBase + '/list');
+        }
+
+        factory.get = function (id) {
+          return $http.get(urlBase + '/' + id);
+        };
+
+        factory.insert = function (product) {
+          return $http.post(urlBase, product);
+        };
+
+        factory.update = function (product) {
+          return $http.put(urlBase + '/' + product.id, product)
+        };
+
+        factory.delete = function (id) {
+          return $http.delete(urlBase + '/' + id);
+        };
+
+        return factory;
+      }
+    ])
+
+  .factory('recipeFactory',
+    [          '$http',
+      function ($http) {
+        var urlBase = '/api/api/v1/recipe';
+        var factory = {};
+
+        factory.getList = function () {
+          return $http.get(urlBase + '/list');
+        }
+
+        factory.get = function (id) {
+          return $http.get(urlBase + '/' + id);
+        };
+
+        factory.insert = function (recipe) {
+          return $http.post(urlBase, recipe);
+        };
+
+        factory.update = function (recipe) {
+          return $http.put(urlBase + '/' + recipe.id, recipe)
+        };
+
+        factory.delete = function (id) {
+          return $http.delete(urlBase + '/' + id);
         };
 
         return factory;
--- js/food.js
+++ js/food.js
@@ -21,33 +21,47 @@ angular.module('uHealth.food', [
             url: '/add',
             templateUrl: PAGE_URL + 'recipes.add.html',
             resolve: {
-              products: ['products',
-                function (products) {
-                  return products.all();
+              products: ['productFactory',
+                function (productFactory) {
+                  return productFactory.getList();
                 }]
             },
-            controller: ['$scope', '$state', 'products',
-              function ($scope, $state, products) {
-                $scope.products = products;
+            controller: function ($scope, products) {
+              $scope.products = products.data.products;
 
-                var INITIAL_INGREDIENTS_COUNT = 5;
-
-                $scope.ingredients = [];
-                for (var i = 0; i < INITIAL_INGREDIENTS_COUNT; i++) {
-                  $scope.ingredients.push({});
-                }
-
-                $scope.addIngredient = function () {
-                  $scope.ingredients.push({});
-                }
-
-                $scope.selectProduct = function () {
-                  angular.extend(this.$parent.ingredient, this.p);
-                  this.showProductList = false;
-                }
-              }]
+              $scope.selectProduct = function () {
+                angular.extend(this.$parent.ingredient, this.p);
+                this.showProductList = false;
+              }
+            }
           });
       }
     ]
-  );
+  )
+  .controller('addRecipeController', function ($scope, recipeFactory) {
+    var INITIAL_INGREDIENTS_COUNT = 5;
+
+    $scope.ingredients = [];
+    for (var i = 0; i < INITIAL_INGREDIENTS_COUNT; i++) {
+      $scope.ingredients.push({});
+    }
+
+    $scope.addIngredient = function () {
+      $scope.ingredients.push({});
+    }
+
+    $scope.saveRecipe = function () {
+      var data = angular.extend($scope.recipe, {
+        ingredients: $scope.ingredients.filter(function($v) { return !!$v.id; })
+      });
+
+      recipeFactory.insert(data)
+        .success(function(data) {
+          console.log(data);
+        })
+        .error(function(error) {
+          console.log(error);
+        })
+    }
+  });
 
